cb8cbd4f245fcd38bae26aacecb76dd92b068a21
AlbogachievM <albogachiev-2014@mail.ru>
2024-03-06 11:02:21 +0300

    completed my homework 5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -9,6 +9,7 @@ type PropsType = {
     handleClose: () => void
 }
 
+
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
@@ -31,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) => isActive ? s.active : '' } // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) => isActive ? s.active : '' } // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,9 +48,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) => isActive ? s.active : '' } // делает студент
                     >
-                        Junior Plus
+                        Junior PLUS
                     </NavLink>
                 </nav>
             </aside>
