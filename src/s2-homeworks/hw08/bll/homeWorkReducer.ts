5acf3766b082d12a58b5dd05b0bb7307fab4eac1
AlbogachievM <albogachiev-2014@mail.ru>
2024-04-16 12:28:37 +0300

    completed 8-homework

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,19 @@
-import {UserType} from '../HW8'
+import {initialPeople, UserType} from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+type initialStateT = typeof initialPeople;
 
-            return state // need to fix
+export const homeWorkReducer = (state: initialStateT = initialPeople, action: ActionType): initialStateT => { // need to fix any
+    switch (action.type) {
+        case 'sort': {
+            return action.payload === 'up' ? state.sort((a, b) => a.name > b.name ? 1 : -1)
+                : action.payload === 'down' ? state.sort((a, b) => a.name < b.name ? 1 : -1) : state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
