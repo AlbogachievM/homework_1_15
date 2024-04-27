70dd1c532ab10798fc07d970d50613c1363f5942
AlbogachievM <albogachiev-2014@mail.ru>
2024-04-27 09:01:07 +0300

    fixed 9hw

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -34,22 +34,19 @@ function Clock() {
         setShow(!show)
     }
 
-
     const sec = date.getSeconds() <= 9 ? '0' + date.getSeconds() : date.getSeconds()
     const min = date.getMinutes() <= 9 ? '0' + date.getMinutes() : date.getMinutes()
     const hour = date.getHours() <= 9 ? '0' + date.getHours() : date.getHours()
 
-    const daysWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    const monthYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-    const day = date.getDate() <= 9 ? '0' + date.getDate() + 1 : date.getDate()
+    const day = date.getDate() <= 9 ? '0' + (date.getDate() + 1) : date.getDate()
     const mount = date.getMonth() <= 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
     const year = date.getFullYear()
+
     const stringTime = `${hour}:${min}:${sec}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${day}:${mount}:${year}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${day}.${mount}.${year}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = daysWeek[date.getDay()] || <br/> // пишут студенты
-    const stringMonth = monthYear[date.getMonth()] || <br/> // пишут студенты
+    const stringDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date) || <br/> // пишут студенты
+    const stringMonth = new Intl.DateTimeFormat('en-US', {month: 'long'}).format(date) || <br/> // пишут студенты
     return (
         <div className={s.clock}>
             <div
